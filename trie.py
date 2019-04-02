dcf2acc58eb025dc588358f602ac3b999e11c5d5
Sacha Ddkn <49188310+Sacha-Ddkn@users.noreply.github.com>
2019-04-02 11:34:27 +0200

    Update and rename test.html to trie.py

---
+++ trie.py
@@ -0,0 +1,8 @@
+
+# Regroupons sur ce fichier les fonctions servant à la création de la trie à partir d'un ensemble de textes
+
+# Le choix des textes doit être modernes pour que l'ensemble des mots et des fréquences de base soit cohérents
+# avec une utilisation actuelle.
+
+# Article "Implémenter une trie dans Python en moins de 100 lignes", plutôt inspirant si on ne sait pas par où démarrer :
+# https://towardsdatascience.com/implementing-a-trie-data-structure-in-python-in-less-than-100-lines-of-code-a877ea23c1a1
